ffeef76db113699148f906858d984db56ca73c41
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2018-12-18 20:03:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,5 @@
 class Song
+  extends concerns::findable
   attr_accessor :name , :artist, :genre
   @@all = []
   def self.all
@@ -24,14 +25,14 @@ class Song
     song.save
     song
   end
-  def self.find_by_name(name)
-    @@all.detect {|song| song.name == name }
-  end
-  def self.find_or_create_by_name(name)
-    if self.find_by_name(name) == nil
-      self.create(name)
-    else
-      self.find_by_name(name)
-    end
-  end
+  #def self.find_by_name(name)
+  #  @@all.detect {|song| song.name == name }
+#  end
+#  def self.find_or_create_by_name(name)
+#    if self.find_by_name(name) == nil
+      #self.create(name)
+  #  else
+      #self.find_by_name(name)
+    #end
+  #end
 end
