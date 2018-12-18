069e65efe7553018907b3a50f3db0447d53eda76
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2018-12-18 21:51:49 +0000

    Automatically backed up by Learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -113,11 +113,11 @@ class MusicLibraryController
   def play_song
     num = 1
     puts "Which song number would you like to play?"
-    ray = Song.all.sort_by{|m| m.name }
-    ray.each do |track|
-      puts "#{num}. #{track.artist.name} - #{track.name} - #{track.genre.name}"
-      num += 1
-    end
+    #ray = Song.all.sort_by{|m| m.name }
+    #ray.each do |track|
+  #    puts "#{num}. #{track.artist.name} - #{track.name} - #{track.genre.name}"
+#      num += 1
+  #  end
 
     song_num = gets.chomp
     if song_num.to_i < 1 || song_num.to_i > ray.size
