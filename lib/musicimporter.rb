24f894f9dbb8d001c440750dd85ba0c3a32670c2
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2018-12-18 21:10:43 +0000

    Automatically backed up by Learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -16,12 +16,15 @@ class MusicImporter
     end
   end
 end
+
 class MusicLibraryController
   attr_accessor :path
   def initialize(path="./db/mp3s/")
     @path = path
   end
+
   def call
 
   end
+
 end
