f5a8395451c9663ba9b656b7ee4c875c014ee7d3
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2018-12-18 20:44:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,14 +37,6 @@ class Song
       self.genre = genre
     end
   end
-  #def self.find_by_name(name)
-  #  @@all.detect {|song| song.name == name }
-#  end
-#  def self.find_or_create_by_name(name)
-#    if self.find_by_name(name) == nil
-      #self.create(name)
-  #  else
-      #self.find_by_name(name)
-    #end
-  #end
+  def new_from_filename(filename)
+    fileshort = filename[0..-5]
 end
