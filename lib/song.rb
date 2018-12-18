446be3369bc7acdd97ddf0aa118b1e2d17d2ab47
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2018-12-18 20:26:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,8 @@ class Song
   def initialize(name, artist=nil, genre=nil)
     @name = name
     if artist != nil
-      artist=(artist)
+      #artist=(artist)
+      @artist = artist
     end
     if genre != nil
       genre=(genre)
