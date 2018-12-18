7247744aada02e1331f0a236fbd3d7752f7b51cd
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2018-12-18 20:17:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,6 +30,10 @@ class Song
     arg.songs << self
     arg.song = arg.songs.uniq
   end
+  def artist=(arg)
+    @artist = arg
+    arg.add_song(self)
+  end
   #def self.find_by_name(name)
   #  @@all.detect {|song| song.name == name }
 #  end
