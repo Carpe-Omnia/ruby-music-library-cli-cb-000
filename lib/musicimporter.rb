ea7b471474d58b2767e21870edd2aefaceb1a306
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2018-12-18 21:49:26 +0000

    Automatically backed up by Learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -119,6 +119,11 @@ class MusicLibraryController
     end
     puts "Which song number would you like to play?"
     song_num = gets.chomp
+    if song_num < 1 || song_num > ray.size
+      self.do_nothing 
+    else 
+      self.do_nothing 
+    end     
   end
 
   def do_nothing
